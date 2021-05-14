608667c00cdf906060f3daca8d982b5043010ab5
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-14 23:00:53 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,23 @@
+//Array of objects
+const quiz = [
+    {
+        q:"'A more detailed examination will be needed to ............... the exact nature of the tumour', said the doctor.",
+        options:["determine", "regulate", "restore", "develop", "reduce"],
+        answer: 0
+    },
+    {
+        q:"The İzmir-Çeşme motorway was blocked because of an accident ............... two lorries.",
+        options:["containing", "involving", "dealing with", "including", "combining"],
+        answer: 1
+    },
+    {
+        q:"His doctor told him that he must stay away from hard work and take it ............... for a couple of weeks after such a serious illness.",
+        options:["comfortably", "medically", "easy", "easily", "comfortable"],
+        answer: 2
+    },
+    {
+        q:"A surprise prize will now be ............... ............... the winner by the Minister himself.",
+        options:["presented / to", "forwarded / toward", "thrown / at", "received / from", "possessed / by"],
+        answer: 0
+    }
+]
